69fd940854623dff8523ed0c78695267f9367d16
Xiao <Wei.Xiao@emc.com>
2019-02-25 11:24:51 +0800

    2nd commit

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"github.com/weiwei695/stringutil"
+)
 func main(){
 	fmt.Printf("Hello, world\n")
+	fmt.Println(stringutil.Reverse("!oG,olleH"))
 }
